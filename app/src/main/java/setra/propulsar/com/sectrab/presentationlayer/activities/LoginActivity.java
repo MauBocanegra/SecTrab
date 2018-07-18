0da7caad6d110eedc9fff19f98f985426239dbf9
givethhcass <36676650+givethhcass@users.noreply.github.com>
2018-07-18 15:09:25 -0500

    LoginActivity

--- app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/LoginActivity.java
+++ app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/LoginActivity.java
@@ -1,17 +1,32 @@
 package setra.propulsar.com.sectrab.presentationlayer.activities;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.TextInputLayout;
+import android.view.View;
+import android.widget.EditText;
 
 import setra.propulsar.com.sectrab.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextInputLayout textInputCorreo;
+    private TextInputLayout textInputContra;
+    private EditText editTextCorreo;
+    private EditText editTextContra;
+
+    // -------------------------------------------- //
+    // ---------------- LIFE CYCLE ---------------- //
+    // -------------------------------------------- //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
