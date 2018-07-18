c7b05a074df43766bb9afe053c11e7f2b095d0ad
givethhcass <36676650+givethhcass@users.noreply.github.com>
2018-07-18 14:38:45 -0500

    LoginActivity

--- app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/RegisterActivity.java
+++ app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/RegisterActivity.java
@@ -1,15 +1,15 @@
-package setra.propulsar.com.sectrab.presentationlayer;
+package setra.propulsar.com.sectrab.presentationlayer.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import setra.propulsar.com.sectrab.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_register);
     }
 }
--- app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/SplashActivity.java
+++ app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/SplashActivity.java
@@ -2,16 +2,14 @@ package setra.propulsar.com.sectrab.presentationlayer.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.TextInputLayout;
 
 import setra.propulsar.com.sectrab.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_splash);
     }
 }
