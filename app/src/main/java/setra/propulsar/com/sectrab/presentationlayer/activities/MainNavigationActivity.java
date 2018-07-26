783eb712f2476088b4b90ddec525c15c9425ba73
givethhcass <36676650+givethhcass@users.noreply.github.com>
2018-07-26 00:12:21 -0500

    Merge branch 'master' of https://github.com/MauBocanegra/SecTrab
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/MainNavigationActivity.java
+++ app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/MainNavigationActivity.java
@@ -1,9 +1,14 @@
 package setra.propulsar.com.sectrab.presentationlayer.activities;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 
@@ -20,19 +25,42 @@ public class MainNavigationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_navigation);
 
         //Agregamos las instancias del Drawer
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_navigation_activity);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_navigation_layout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_navigation_activity,menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
+
+        int id = item.getItemId();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+
         if (mToggle.onOptionsItemSelected(item)){
             return true;
+        } else if (id == R.id.nav_noticias){
         }
+
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_drawer_navigation_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else { super.onBackPressed();}
+    }
+
+
 }
