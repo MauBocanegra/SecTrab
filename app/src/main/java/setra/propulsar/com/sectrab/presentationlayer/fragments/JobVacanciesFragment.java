5219a447ed10baebed7bbfd21597ef3543d2a928
givethhcass <36676650+givethhcass@users.noreply.github.com>
2018-08-03 00:35:43 -0500

    Merge branch 'master' of https://github.com/MauBocanegra/SecTrab
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/fragments/JobVacanciesFragment.java
+++ app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/fragments/JobVacanciesFragment.java
@@ -2,19 +2,50 @@ package setra.propulsar.com.sectrab.presentationlayer.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import setra.propulsar.com.sectrab.R;
+import java.util.ArrayList;
 
-public class JobVacanciesFragment extends Fragment {
+import setra.propulsar.com.sectrab.R;
+import setra.propulsar.com.sectrab.domainlayer.adapters.JobsAdapter;
+import setra.propulsar.com.sectrab.domainlayer.models.Jobs;
+
+public class JobVacanciesFragment extends Fragment  implements SwipeRefreshLayout.OnRefreshListener {
+
+    private RecyclerView mRecyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private JobsAdapter mAdapter;
+    private ArrayList<Jobs> mDataset;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_job_vacancies, container, false);
+        View view = inflater.inflate(R.layout.fragment_job_vacancies, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.fragmentjobs_recyclerview);
+        mRecyclerView.setHasFixedSize(true);
+
+        mDataset = new ArrayList<Jobs>();
+
+        mAdapter = new JobsAdapter(mDataset, getContext());
+        mRecyclerView.setAdapter(mAdapter);
+        mSwipeRefreshLayout=view.findViewById(R.id.swipeRefreshLayoutFragJobs);
+
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mSwipeRefreshLayout.setOnRefreshListener(JobVacanciesFragment.this);
+
+        return view;
     }
 
+    @Override
+    public void onRefresh() {
+
+    }
 }
