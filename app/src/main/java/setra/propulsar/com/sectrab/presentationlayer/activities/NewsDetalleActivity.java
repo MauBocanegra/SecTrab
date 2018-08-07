abef413142e27ea34baecfc2b320d4c25ff2ba49
givethhcass <36676650+givethhcass@users.noreply.github.com>
2018-08-07 01:30:35 -0500

    Merge branch 'master' of https://github.com/MauBocanegra/SecTrab
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/setra/propulsar/com/sectrab/presentationlayer/activities/NewsDetalleActivity.java
@@ -0,0 +1,15 @@
+package setra.propulsar.com.sectrab.presentationlayer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import setra.propulsar.com.sectrab.R;
+
+public class NewsDetalleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_detalle);
+    }
+}
